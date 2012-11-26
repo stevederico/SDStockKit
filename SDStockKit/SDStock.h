de551064640307082d5aa0f67d3e450ad32943e8
Steve Derico <steve@stevederico.com>
2012-11-26 11:34:11 -0800

    Added AFNetworking Submodule

---
+++ SDStockKit/SDStock.h
@@ -0,0 +1,13 @@
+//
+//  SDStock.h
+//  SimpleStocks
+//
+//  Created by Steve Derico on 11/26/12.
+//  Copyright (c) 2012 Bixby Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDStock : NSObject
+
+@end
---
+++ SDStockKit/SDStock.m
@@ -0,0 +1,13 @@
+//
+//  SDStock.m
+//  SimpleStocks
+//
+//  Created by Steve Derico on 11/26/12.
+//  Copyright (c) 2012 Bixby Apps. All rights reserved.
+//
+
+#import "SDStock.h"
+
+@implementation SDStock
+
+@end
